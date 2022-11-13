bc6efbd5581f4ad6970a65dce7c1db98494f378d
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-13 16:25:42 -0600

    Time: 265 ms (26.27%), Space: 18.6 MB (91.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        temp = {}
-        val_set = set()
-        result = []
+        
+        hash_map = {}
+        rslt = []
         for i in nums:
-            if i not in temp:
-                temp[i] = 1
+            if i not in hash_map:
+                hash_map[i] = 1
             else:
-                temp[i] += 1
-        for key,val in temp.items():
-            val_set.add((key,val))
-            if len(val_set) > k:
-                val_set.remove(min(val_set,key = lambda x:x[1]))
-        for items in val_set:
-            result.append(items[0])
-        return result
+                hash_map[i] += 1
+        
+        rslt = sorted(hash_map, key = lambda x: hash_map[x], reverse = True)
+        return rslt[:k]
