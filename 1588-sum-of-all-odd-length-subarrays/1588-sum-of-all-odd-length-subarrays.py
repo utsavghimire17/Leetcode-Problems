fcd3ead83c836f346483d56a608e842feb1b9ed7
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-17 23:52:37 -0600

    Time: 45 ms (82.03%), Space: 13 MB (99.71%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -6,10 +6,10 @@ class Solution(object):
         """
         curr_sum = 0
         max_sum = 0
-        for i in range(len(arr)-2):
+        for i in range(len(arr)):
             curr_sum = 0
             for j in range(i,len(arr)):
                 curr_sum += arr[j]
                 if (j - i + 1) % 2 == 1:
                     max_sum += curr_sum
-        return max_sum + sum(arr[len(arr)-2:])
+        return max_sum 
