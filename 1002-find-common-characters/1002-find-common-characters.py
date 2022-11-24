fa63a8f218b525d4328130fc8c5700560e45d3d2
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-23 23:43:39 -0600

    Time: 172 ms (6.21%), Space: 14.1 MB (26.96%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,34 +1,16 @@
-class Solution(object):
-    def commonChars(self, words):
-        """
-        :type words: List[str]
-        :rtype: List[str]
-        """
-        old_dict = {}
-        new_dict = {}
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        minimum_words = [float("inf")] * 26
         res = []
-        new_str = ""
-        for char in words[0]:
-            if char not in old_dict:
-                old_dict[char] = 1
-            else:
-                old_dict[char] += 1
-
-        for idx in range(1,len(words)):
-            for j in range(len(words[idx])):
-                if words[idx][j] in old_dict and old_dict[words[idx][j]] > 0:
-                    if words[idx][j] not in new_dict:
-                        new_dict[words[idx][j]] = 1
-                    else:
-                        new_dict[words[idx][j]] += 1
-                    old_dict[words[idx][j]] -= 1
-            if idx == len(words) - 1:
-                break
-            old_dict = new_dict
-            new_dict = {}
-        for char in new_dict:
-            new_str = (char*new_dict[char])
-            res.extend(new_str)
-        return res
-            
-                        
+        for word in words:
+            current_words = [0] * 26
+            for char in word:
+                current_words[ord(char) - ord("a")] += 1
+                
+            for i in range(len(current_words)):
+                minimum_words[i] = min(minimum_words[i],current_words[i])
+        
+        for i in range(len(minimum_words)):
+            if minimum_words[i] != 0:
+                res.extend(chr(97+i)*minimum_words[i])
+        return res 
