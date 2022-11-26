73877c44d7095c0cd85b82760287b69203289756
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-26 00:58:27 -0600

    Time: 43 ms (10.25%), Space: 13.3 MB (81.99%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        str1 = s + s
+        if goal in str1:
+            return True
+        return False
+        
