27fa4437aada8771ebafe710a106f70a9b9cb7dc
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-27 20:56:06 -0600

    Time: 2383 ms (10.12%), Space: 17.8 MB (60.40%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        temp = {}
+        end = 0
+        diff = 0
+        for nums in bills:
+            temp[nums] = 1 + temp.get(nums,0)
+            if nums - 5 != 0:
+                if nums - 5 == 5:
+                    if nums - 5 in temp:
+                        if temp[nums-5] >= (nums-5)//5:
+                            temp[nums-5] -= (nums-5)//5
+                        else:
+                            return False
+                    else:
+                        return False
+                elif nums - 5 > 5:
+                    if 10 in temp and temp[10] >= 1:
+                        if 5 in temp and temp[5] >= 1:
+                            temp[10] -= 1
+                            temp[5] -=1
+                        else:
+                            return False
+                    elif 5 in temp:
+                        if temp[5] >= (nums-5)//5:
+                            temp[5] -= (nums-5)//5
+                        else:
+                            return False
+                    else:
+                        return False
+
+        return True
+
