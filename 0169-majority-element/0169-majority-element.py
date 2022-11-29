17ea12d5f07984b675f403844019e95289587bb9
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-28 19:48:42 -0600

    Time: 208 ms (78.50%), Space: 15.5 MB (86.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        temp = {}
-        n = len(nums)
-        for i in nums:
-            temp[i] = 1 + temp.get(i,0)
-        for val in temp:
-            if temp[val] > n//2 :
-                return val
+        end = 0
+        start = 0
+        max_val = 0
+        nums.sort()
+        while end < len(nums):
+            if nums[start] == nums[end]:
+                max_val = max(max_val,end-start+1)
+                if max_val > len(nums)//2:
+                    return nums[start]
+                end += 1
+            elif nums[start] != nums[end]:
+                start = end
