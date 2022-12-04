d2e168de22ef358692164c6332db106519edc589
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-04 00:02:19 -0600

    Time: 234 ms (61.04%), Space: 18.2 MB (64.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         new_intervals = sorted(intervals, key = lambda x:x[0])
         res = []
-        val = True
         temp = new_intervals[0]
         i = 0
         if len(new_intervals) == 1:
@@ -14,17 +13,12 @@ class Solution(object):
         while i < len(new_intervals):
             if temp[1] in range(new_intervals[i][0],new_intervals[i][1]+1):
                 temp = [temp[0],new_intervals[i][1]]
-                val = True
-                i += 1
             elif new_intervals[i][1] in range(temp[0],temp[1]):
-                val = True
                 temp = [temp[0],temp[1]]
-                i += 1
             else:
-                val = False
                 res.append(temp)
                 temp = new_intervals[i]
-                i += 1
+            i += 1
         else:
             res.append(temp)
         return res
