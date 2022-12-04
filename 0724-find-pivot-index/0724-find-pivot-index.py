d9aabf20c0294d056f776179a4b7df31f1c45c02
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-04 17:49:08 -0600

    Time: 262 ms (62.72%), Space: 14.6 MB (41.89%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,15 +1,16 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        prefix_arr = []
-        postfix_arr = []
-        curr_sum = 0
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         total = sum(nums)
-        for i in range(len(nums)):
-            prefix_arr.append(curr_sum)
-            postfix_arr.append(total-nums[i])
-            curr_sum += nums[i]
-            total -= nums[i]
-        for idx in range(len(prefix_arr)):
-            if prefix_arr[idx] == postfix_arr[idx] and prefix_arr != 0:
-                return idx
+        total1 = 0
+        if total == nums[0]:
+            return 0
+        for index in range(len(nums)-1):
+            total1 += nums[index]
+            if total - nums[index+1] == 2*total1:
+                return index+1
+            
         return -1
