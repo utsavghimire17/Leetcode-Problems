a0cb62bb7bf16929739d16b581f7e49b607e3d0a
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-06 17:25:49 -0600

    Time: 70 ms (49.00%), Space: 16.5 MB (8.91%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,19 +1,15 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-#         Created a counter map to keep track of number of elements and char_map to map the character of s to character of t.
-        temp_s = {}
-        temp_t = {}
-        char_map = {}
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)):
-            temp_s[s[i]] = 0 + temp_s.get(s[i],0)
-            temp_t[t[i]] = 0 + temp_t.get(t[i],0)
-        for idx in range(len(s)):
-            temp_s[s[idx]] += 1
-            temp_t[t[idx]] += 1
-            if s[idx] not in char_map:
-                char_map[s[idx]] = t[idx]
-            if temp_s[s[idx]] != temp_t[t[idx]] or char_map[s[idx]] != t[idx]:
-                   return False
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        map_s = {}
+        map_t = {}
+        for c1,c2 in zip(s,t):
+            if (c1 in map_s and map_s[c1] != c2) or (c2 in map_t and map_t[c2] != c1):
+                return False
+            map_s[c1] = c2
+            map_t[c2] = c1
         return True
