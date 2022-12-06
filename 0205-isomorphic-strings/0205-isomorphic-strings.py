a7ced5edec31ce5deb47f43c62f23d9feb52896c
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-06 16:58:19 -0600

    Time: 194 ms (5.01%), Space: 14.3 MB (20.97%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        temp_s = {}
+        temp_t = {}
+        char_map = {}
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            temp_s[s[i]] = 0 + temp_s.get(s[i],0)
+            temp_t[t[i]] = 0 + temp_t.get(t[i],0)
+        for idx in range(len(s)):
+            temp_s[s[idx]] += 1
+            temp_t[t[idx]] += 1
+            if s[idx] not in char_map:
+                char_map[s[idx]] = t[idx]
+            if temp_s[s[idx]] != temp_t[t[idx]] or char_map[s[idx]] != t[idx]:
+                   return False
+        return True
