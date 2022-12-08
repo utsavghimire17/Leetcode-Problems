0a1d18c397b0db134b390874cb63c0f4b794bfa7
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-08 02:29:00 -0600

    Time: 22 ms (86.93%), Space: 13.4 MB (37.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -12,14 +12,13 @@ class Solution(object):
             if nums[start] == val:
                 if nums[end] == val:
                     end += 1
+                    continue
                 else:
-                    nums[start],nums[end] = nums[end], nums[start]
-                    start += 1
-                    end += 1
-            else:
-                count += 1
-                start += 1
-                end += 1
+                    nums[start], nums[end] = nums[end],nums[start]
+                    
+            start += 1
+            end += 1
+        
         for i in range(len(nums)-1,-1,-1):
             if nums[i] == val:
                 nums.pop()
