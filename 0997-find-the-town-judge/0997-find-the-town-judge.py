e5b1cba863317893d803a27b1e66b9819a9d94c8
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-11 15:11:47 -0600

    Time: 779 ms (87.67%), Space: 19.1 MB (7.30%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,14 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         count = [0]*(n+1)
-        if len(trust) == 0:
-            if n % 2 == 0:
-                return -1
-            return 1
         for i in range(len(trust)):
             count[trust[i][1]] += 1
             count[trust[i][0]] -= 1
-        for i in range(len(count)):
+        for i in range(1,len(count)):
             if count[i] == n-1:
                 return i
         return -1
