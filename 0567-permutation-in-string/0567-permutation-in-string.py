bed834922fe8801d507644e7d124dc233745c1bf
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-13 16:40:28 -0600

    Time: 84 ms (89.36%), Space: 14 MB (68.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,25 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-#         
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         temp = {}
-        start = 0
-        matched = 0
-        for char in s1:
-            if char not in temp:
-                temp[char] = 1
-            elif char in temp:
-                temp[char] += 1
-        for i in range(len(s2)):
-            if s2[i] in temp:
-                temp[s2[i]] -= 1
-                if temp[s2[i]] == 0:
-                    matched += 1
-            if i >= len(s1) and s2[i - len(s1)] in temp:
-                if temp[s2[i-len(s1)]] == 0:
-                    matched -= 1
-                temp[s2[i-len(s1)]] += 1
+        start = 0 
+        end = 0
+        matches = 0
+        for i in s1:
+            temp[i] = 1 + temp.get(i,0)
+        while end < len(s2):
+            if s2[end] in temp:
                 
-                     
-            if matched == len(temp):
-                     return True
+                temp[s2[end]] -= 1
+                if temp[s2[end]] == 0:
+                    matches += 1
+            if end - start + 1 >= len(s1):
+                if start > 0 and s2[start - 1] in temp:
+                    if temp[s2[start - 1]] == 0:
+                        matches -= 1
+                    temp[s2[start - 1]] += 1
+                    
+                start += 1
+            if matches == len(temp):
+                return True
+            end += 1
         return False
