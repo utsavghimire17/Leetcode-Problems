73bbd12d4fb3fff135982afcd7920f27ee641186
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-13 17:21:39 -0600

    Time: 68 ms (96.12%), Space: 14 MB (31.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,25 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        temp = {}
-        start = 0 
-        end = 0
-        matches = 0
+#         temp = {}
+#         start = 0 
+#         end = 0
+#         matches = 0
+#         for i in s1:
+#             temp[i] = 1 + temp.get(i,0)
+#         while end < len(s2):
+#             if s2[end] in temp:
+
+#                 temp[s2[end]] -= 1
+#                 if temp[s2[end]] == 0:
+#                     matches += 1
+#             if end - start + 1 >= len(s1):
+#                 if start > 0 and s2[start - 1] in temp:
+#                     if temp[s2[start - 1]] == 0:
+#                         matches -= 1
+#                     temp[s2[start - 1]] += 1
+
+#                 start += 1
+#             if matches == len(temp):
+#                 return True
+#             end += 1
+#         return False
+        temp1 = {}
+        temp2 = {}
+        start = 0
         for i in s1:
-            temp[i] = 1 + temp.get(i,0)
-        while end < len(s2):
-            if s2[end] in temp:
-                
-                temp[s2[end]] -= 1
-                if temp[s2[end]] == 0:
-                    matches += 1
-            if end - start + 1 >= len(s1):
-                if start > 0 and s2[start - 1] in temp:
-                    if temp[s2[start - 1]] == 0:
-                        matches -= 1
-                    temp[s2[start - 1]] += 1
-                    
+            temp1[i] = 1 + temp1.get(i,0)
+        for alp in "abcdefghijklmnopqrstuvwxyz":
+            if alp not in temp1:
+                temp1[alp] = 0
+            temp2[alp] = 0
+        for j in range(len(s2)):
+            temp2[s2[j]] = 1 + temp2.get(s2[j],0)
+            if j - start + 1 == len(s1):
+                if temp2 == temp1:
+                    return True
+                temp2[s2[start]] -= 1
                 start += 1
-            if matches == len(temp):
-                return True
-            end += 1
         return False
+        
