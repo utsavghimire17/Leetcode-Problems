c6488088711fe108fd90a3c65a69f3268e99ad34
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-12-18 03:00:59 -0600

    Time: 50 ms (93.45%), Space: 14.1 MB (96.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        res = []
-        index = {}
-        for i in range(len(nums2)):
-            index[nums2[i]] = i
-        for j in nums1:
-            val = False
-            for k in range(index[j] + 1,len(nums2)):
-                if nums2[k] > j:
-                    val = True
-                    res.append(nums2[k])
-                    break
-            if not val: 
-                res.append(-1)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        temp = {}
+        res = [-1] * len(nums1)
+        for idx,elem in enumerate(nums1):
+            temp[elem] = idx
+        for num in nums2:
+            while stack and num > stack[-1]:
+                if stack[-1] in temp:
+                    res[temp[stack[-1]]] = num
+                stack.pop()
+            stack.append(num)
         return res
