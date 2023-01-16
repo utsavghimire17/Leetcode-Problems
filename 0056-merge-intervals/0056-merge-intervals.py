1981471dac6bbe4435da9b454e018370e0a85e0d
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2023-01-15 18:06:59 -0600

    Time: 149 ms (87.00%), Space: 18.1 MB (52.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        new_intervals = sorted(intervals, key = lambda x:x[0])
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_intervals = sorted(intervals)
         res = []
-        temp = new_intervals[0]
-        i = 0
-        if len(new_intervals) == 1:
-            return [temp]
-        while i < len(new_intervals):
-            if temp[1] in range(new_intervals[i][0],new_intervals[i][1]+1):
-                temp = [temp[0],new_intervals[i][1]]
-            elif new_intervals[i][1] in range(temp[0],temp[1]):
-                temp = [temp[0],temp[1]]
+        temp = sorted_intervals[0]
+        i =1 
+        while i < len(sorted_intervals):
+            if temp[1] >= sorted_intervals[i][0]:
+                if temp[1] >= sorted_intervals[i][1]:
+                    temp = [temp[0],temp[1]]
+                else:
+                    temp = [temp[0],sorted_intervals[i][1]]
             else:
                 res.append(temp)
-                temp = new_intervals[i]
+                temp = sorted_intervals[i]
             i += 1
-        else:
+        if temp:
             res.append(temp)
         return res
