bfc9071d6e0df41f1d47e96f04ff29df5806ab79
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2023-03-02 12:42:46 -0600

    Time: 34 ms (69.02%), Space: 13.8 MB (60.97%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        queue = collections.deque()
+        res = []
+        if root is None:
+            return []
+        queue.append(root)
+        while queue:
+            length = len(queue)
+            for i in range(length):
+                node = queue.popleft()
+                if i == length - 1:
+                    res.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return res
