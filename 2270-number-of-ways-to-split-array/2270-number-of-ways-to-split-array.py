6c05b941136b7b52c54979a97f962091e076744d
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2023-03-06 16:56:57 -0600

    Time: 980 ms (53.14%), Space: 30.2 MB (5.10%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -1,18 +1,17 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        prefix_arr = []
-        postfix_arr = []
-        curr_sum = 0
+        prefix = []
+        postfix = []
         total = sum(nums)
+        curr_sum = 0
         count = 0
-        for i in range(len(nums)):
-            curr_sum += nums[i]
-            prefix_arr.append(curr_sum)
-            postfix_arr.append(total-nums[i])
-            
-            total -= nums[i]
-            
-        for idx in range(len(prefix_arr)-1):
-            if prefix_arr[idx] >= postfix_arr[idx]:
+        for num in nums:
+            curr_sum += num
+            prefix.append(curr_sum)
+            postfix.append(total - num)
+            total -= num
+        for idx in range(len(nums) - 1):
+            if prefix[idx] >= postfix[idx]:
                 count += 1
-        return count     
+        return count
+            
