ef2da816ceb4b9bbbaaf4b3017082cdd32cafa98
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-23 19:59:48 -0500

    Time: 207 ms (88.25%), Space: 14.7 MB (92.23%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,12 +1,39 @@
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        start = 0
-        end = len(arr) - k
-        mid = 0
-        while start < end:
-            mid = (start + end) // 2
-            if x - arr[mid] > arr[mid + k] - x:
-                start = mid + 1
-            else:
-                end = mid
-        return arr[start : start + k]
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        def binary_search(arr, k, x):
+            start = 0
+            end = len(arr) - 1
+            mid = 0
+            while start <= end:
+                mid = (start + end)//2
+                if arr[mid] == x:
+                    return mid 
+                elif arr[mid] > x:
+                    end = mid - 1
+                elif arr[mid] < x:
+                    start = mid + 1
+            return mid
+
+        def closest_number(arr, k, x):
+            mid_index = binary_search(arr, k, x)
+            start = mid_index - k
+            end = mid_index + k
+            if mid_index - k < 0:
+                start = 0
+            if mid_index + k >= len(arr):
+                end = len(arr) - 1
+            while end - start + 1 > k:
+                if abs(arr[start] - x) <= abs(arr[end] - x):
+                    end -= 1
+                else:
+                    start += 1
+            return arr[start:end+1] 
+        binary_search(arr,k,x)
+        return closest_number(arr,k,x)
+                
