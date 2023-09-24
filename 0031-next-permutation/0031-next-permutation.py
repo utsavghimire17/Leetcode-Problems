dcd87778fd6767700ee87722e6ab69298ecd49e1
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-24 15:55:49 -0500

    Time: 47 ms (59.38%), Space: 16.2 MB (66.73%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,27 +1,23 @@
-class Solution(object):
-    def nextPermutation(self, nums):
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        i = len(nums)-2
-        break_index = -1
-        while i >= 0:
+        break_point = -1
+        for i in range(len(nums)-2,-1,-1):
             if nums[i] < nums[i+1]:
-                break_index = i
+                break_point = i
                 break
-            i -= 1
-        if break_index == -1:
-            return nums.reverse()
-        for j in range(len(nums)-1,-1,-1):
-            if nums[j] > nums[break_index]:
-                nums[j],nums[break_index] = nums[break_index],nums[j]
+        if break_point == -1:
+            nums.reverse()
+            return
+        for j in range(len(nums)-1,break_point,-1):
+            if nums[j] > nums[break_point]:
+                nums[break_point], nums[j] = nums[j], nums[break_point]
                 break
-                
-        start = break_index + 1
+        start = break_point + 1
         end = len(nums) - 1
         while start < end:
-            nums[start], nums[end] = nums[end],nums[start]
+            nums[start],nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
-        
