04da011ef3ab2fbb4b14496d95de8223f9533b79
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-25 17:04:27 -0500

    Time: 161 ms (29.18%), Space: 16.3 MB (77.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,43 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-#         temp = {}
-#         start = 0 
-#         end = 0
-#         matches = 0
-#         for i in s1:
-#             temp[i] = 1 + temp.get(i,0)
-#         while end < len(s2):
-#             if s2[end] in temp:
-
-#                 temp[s2[end]] -= 1
-#                 if temp[s2[end]] == 0:
-#                     matches += 1
-#             if end - start + 1 >= len(s1):
-#                 if start > 0 and s2[start - 1] in temp:
-#                     if temp[s2[start - 1]] == 0:
-#                         matches -= 1
-#                     temp[s2[start - 1]] += 1
-
-#                 start += 1
-#             if matches == len(temp):
-#                 return True
-#             end += 1
-#         return False
-        temp1 = {}
-        temp2 = {}
+        temp_s1 = {}
+        temp_s2 = {}
         start = 0
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            temp_s1[i] = 0
+            temp_s2[i] = 0
         for i in s1:
-            temp1[i] = 1 + temp1.get(i,0)
-        for alp in "abcdefghijklmnopqrstuvwxyz":
-            if alp not in temp1:
-                temp1[alp] = 0
-            temp2[alp] = 0
-        for j in range(len(s2)):
-            temp2[s2[j]] = 1 + temp2.get(s2[j],0)
-            if j - start + 1 == len(s1):
-                if temp2 == temp1:
+            temp_s1[i] = 1 + temp_s1.get(i,0)
+        for i in range(len(s2)):
+            temp_s2[s2[i]] = 1 + temp_s2.get(s2[i],0)
+            if i - start + 1 == len(s1):
+                print(temp_s2)
+                if temp_s1 == temp_s2:
                     return True
-                temp2[s2[start]] -= 1
+                temp_s2[s2[start]] -= 1
                 start += 1
         return False
+        
         
