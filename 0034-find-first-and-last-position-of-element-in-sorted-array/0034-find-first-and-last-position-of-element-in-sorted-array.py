eeee46bd7f97db268cb22d17f33d95c443d18d7b
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-24 20:08:24 -0500

    Time: 61 ms (31.41%), Space: 14.5 MB (21.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,25 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        left = self.bin_search(nums,target,True)
-        right = self.bin_search(nums,target,False)
-        return [left,right]
-    
-    def bin_search(self,nums,target,leftbias):
-        l = 0
-        r = len(nums) - 1
-        i = -1
-        while l <= r:
-            m = (l + r) // 2
-            if nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
-                l = m + 1
-            else:
-                i = m
-                if leftbias:
-                    r = m - 1
-                else:
-                    l = m + 1
-        return i
-          
+        def binary_search_right(nums,target):
+            start = 0
+            end = len(nums) - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] == target:
+                    if mid + 1 == len(nums) or nums[mid+1] != target:
+                        return mid
+                    else:
+                        start = mid + 1
+                if nums[mid] < target:
+                    start = mid + 1
+                elif nums[mid] > target:
+                    end = mid - 1
+            return -1
+        
+        def binary_search_left(nums,target):
+            start = 0
+            end = len(nums) - 1
+            while start <= end:
+                mid = (start + end) // 2
+                if nums[mid] == target:
+                    if mid - 1 == -1 or nums[mid-1] != target:
+                        return mid
+                    else:
+                        end = mid - 1
+                if nums[mid] < target:
+                    start = mid + 1
+                elif nums[mid] > target:
+                    end = mid - 1
+            return -1 
+        start_index = binary_search_left(nums,target)
+        end_index = binary_search_right(nums,target)
+        return [start_index, end_index]
