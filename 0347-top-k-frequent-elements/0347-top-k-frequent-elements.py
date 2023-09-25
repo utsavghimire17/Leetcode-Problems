4b9ab520161e42afe4733c72aa2a06e1ca8dfdb9
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-25 11:02:33 -0500

    Time: 95 ms (76.79%), Space: 21.1 MB (63.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         temp = {}
-        val_set = set()
-        result = []
-        for i in nums:
-            if i not in temp:
-                temp[i] = 1
-            else:
-                temp[i] += 1
-        for key,val in temp.items():
-            val_set.add((key,val))
-            if len(val_set) > k:
-                val_set.remove(min(val_set,key = lambda x:x[1]))
-        for items in val_set:
-            result.append(items[0])
-        return result
+        frequency = []
+        res = []
+        for num in nums:
+            temp[num] = 1 + temp.get(num,0)
+        for keys,values in temp.items():
+            frequency.append((keys,values))
+        frequency.sort(key=lambda x:x[1], reverse = True)
+        for idx in range(k):
+            res.append(frequency[idx][0])
+        return res
+            
