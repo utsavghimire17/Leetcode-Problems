a62f2c50f203a8ba15f7b32304fcfef6a3294f74
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-25 20:12:33 -0500

    Time: 749 ms (67.33%), Space: 39.8 MB (17.05%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,14 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        speed_track = {}
-        for idx in range(len(position)):
-            speed_track[position[idx]] = speed[idx]
-        position.sort()
+        time = {}
         stack = []
-        for cars in position:
-            while stack and speed_track[stack[-1]] > speed_track[cars]:
-                if cars + (speed_track[cars] * ((cars - stack[-1])/(speed_track[stack[-1]] - speed_track[cars])) ) > target:
-                    break
+        for pos in range(len(position)):
+            time[position[pos]] = (target - position[pos]) / speed[pos]
+        position.sort()
+        for pos in position:
+            while stack and time[stack[-1]] <= time[pos]:
                 stack.pop()
-            stack.append(cars)
+            stack.append(pos)
         return len(stack)
