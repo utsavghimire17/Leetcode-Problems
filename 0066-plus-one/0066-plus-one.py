3ad8cad5097dc7ad37426718557a0d6f923e3a12
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-27 17:17:31 -0500

    Time: 39 ms (64.47%), Space: 16.2 MB (84.95%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        val = False
-        res = [1]
+        carry = 1
+        res = []
         for i in range(len(digits)-1,-1,-1):
-            if digits[i] == 9:
-                digits[i] = 0
-            elif digits[i] != 9:
-                digits[i] = digits[i] + 1
-                val = True
-                break
-        if val == False:
-            res.extend(digits)
-            return res
-        return digits
+            if carry:
+                if digits[i] == 9:
+                    res.append(0)
+                else:
+                    res.append(digits[i] + carry)
+                    carry = 0
+            else:
+                res.append(digits[i])
+        if carry:
+            res.append(1)
+        return res[::-1]
