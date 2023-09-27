a3a93240f51e26e9ab7ca3d9d4bd6ff5378a63af
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-26 20:38:06 -0500

    Time: 76 ms (72.08%), Space: 25.9 MB (97.20%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,36 +1,38 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reorderList(self, head):
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
         """
-        :type head: ListNode
-        :rtype: None Do not return anything, modify head in-place instead.
+        Do not return anything, modify head in-place instead.
         """
-        array = []
-        res = []
-        curr = head
+        #middle_node
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #reverse in_place
+        prev = None
+        curr = slow
         while curr:
-            array.append(curr.val)
-            curr = curr.next
-        start = 0
-        end = len(array) - 1
-        while start < end:
-            res.append(array[start])
-            res.append(array[end])
-            start += 1
-            end -= 1
-        if start == end:
-            res.append(array[start])
-        print(res)
-        if res:
-            new_head = ListNode(res[0])
-            curr = head
-        for i in range(1,len(res)):
-            new_node = ListNode(res[i])
-            print(new_node.val)
-            curr.next = new_node
-            curr = curr.next
-        return new_head
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+       
+        #merge
+        new_curr = head
+        
+        while new_curr != None:
+            temp1 = new_curr.next
+            temp2 = prev.next
+            new_curr.next = prev
+            prev.next = temp1
+            new_curr = temp1
+            prev = temp2
+            
+      
