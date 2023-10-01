8532ff907f37c3bcc052ef515e694e27666f5c94
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-09-30 21:55:53 -0500

    Time: 38 ms (57.02%), Space: 16.1 MB (97.51%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,32 +1,8 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        start = 0
-        end = len(needle) - 1
-        index = -1
-        n = 0
-        while end < len(haystack) and n < len(needle):
-            if haystack[start] == needle[n]:
-                index = start
-                for char in range(start, end + 1):
-                    if n == len(needle):
-                        break
-                    if haystack[char] == needle[n]:
-                        n += 1
-                    else:
-                        start += 1
-                        end += 1
-                        n = 0
-                        break
-            elif haystack[start] != needle[n]:
-                index = -1
-                n = 0
-                start += 1
-                end += 1
-        if n == len(needle):
-            return index
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == needle:
+            return 0
+        for i in range(len(haystack)-len(needle) + 1):
+            if haystack[i:len(needle)+i] == needle:
+                return i
         return -1
