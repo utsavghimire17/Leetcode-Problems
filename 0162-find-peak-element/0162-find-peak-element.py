ef129acbd601d84eb8b233ec919f7914e928d09f
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-10-02 23:59:33 -0500

    Time: 54 ms (38.14%), Space: 16.4 MB (49.42%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,16 +1,3 @@
-class Solution(object):
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid + 1] < nums[mid]:
-                right = mid 
-            else:
-                left = mid +1
-        return left
-        
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        return nums.index(max(nums))
