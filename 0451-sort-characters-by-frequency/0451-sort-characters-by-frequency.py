d9fc89eae7b98fca3afae23e5529eb71170e134a
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-10-04 20:36:21 -0500

    Time: 46 ms (78.46%), Space: 17.5 MB (80.28%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,13 +3,13 @@ class Solution:
         counter = {}
         result = ""
         res = []
+        heapq.heapify(res)
         for i in s:
             counter[i] = 1 + counter.get(i,0)
         for keys,values in counter.items():
-            res.append([keys,values])
-        res.sort(key = lambda x:x[1], reverse = True)
-        print(res)
-        for val in res:
-            result += val[0] * val[1]
+            heapq.heappush(res,(-1 * values,keys))
+        while res:
+            val = heapq.heappop(res)
+            result += (-1*val[0]) * val[1]
         return result
             
