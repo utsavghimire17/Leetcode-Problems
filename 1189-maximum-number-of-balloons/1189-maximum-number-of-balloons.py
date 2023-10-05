cf95f78d886c89f5c312e47fd25c74a41c02042b
Utsav Ghimire <102249506+utsavghimire17@users.noreply.github.com>
2023-10-04 23:17:51 -0500

    Time: 36 ms (85.16%), Space: 16.3 MB (82.26%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -9,8 +9,6 @@ class Solution:
             return 0
         for char in temp:
             if char == "a" or char == "b" or char == "n":
-                if temp[char] < 1:
-                    return 0
                 min_val = min(min_val, temp[char])
             elif char == "l" or char == "o":
                 if temp[char] < 2:
